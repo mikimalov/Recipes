feae17b4b9c6c402eae3c70e125a09677bcb5fde
Miki <miki.stomatologija@gmail.com>
2020-08-07 18:11:04 +0200

    Index page modified, created layout of recipes.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,22 +1,47 @@
 class RecipesController < ApplicationController
   def index
+    @recipes = Recipe.all
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def create
+    @recipe = Recipe.new(recipes_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new
+    end
   end
 
   def edit
+    @recipe = Recipe.find(params[:id])
   end
 
   def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipes_params)
+      redirect_to @recipe
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def recipes_params
+    params.require(:recipe).permit(:title, :description)
   end
 end
