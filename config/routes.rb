d65a4421091b87acad408057da45c331e8aa6093
Miki <miki.stomatologija@gmail.com>
2020-08-07 22:09:17 +0200

    Users controller and User model generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'recipes#index'
 
+  resources :users
   resources :recipes
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_151914) do
+ActiveRecord::Schema.define(version: 2020_08_07_190039) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "full_name"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
