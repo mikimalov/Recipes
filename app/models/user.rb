b5254138bb3cc56d92ef3aba9837d0026c7d1d17
Miki <miki.stomatologija@gmail.com>
2020-08-07 22:19:44 +0200

    User validations and its relations with recipes created.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  has_many :recipes, dependent: :destroy
+
+  validates :full_name, presence: true, length: {minimum: 10}
+  validates :first_name, :last_name, presence: true
+  validates :email, uniqueness: true, presence: true, format: {with: EMAIL_REGEX }
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_190039) do
+ActiveRecord::Schema.define(version: 2020_08_07_201110) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +30,5 @@ ActiveRecord::Schema.define(version: 2020_08_07_190039) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "recipes", "users"
 end
